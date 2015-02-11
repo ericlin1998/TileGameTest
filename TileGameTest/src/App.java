161bf86dab410b562c87a30f60d3910c446a8709
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2015-02-11 08:42:29 -0800

    1st commit

---
+++ TileGameTest/src/App.java
@@ -0,0 +1,29 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class App extends JFrame{
+	private static final long serialVersionUID = 0;
+
+	public App(){
+		initUI();
+	}
+	
+	public void initUI(){
+		add(new Board());
+		setSize(250,200);
+		setTitle("App");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				App app = new App();
+				app.setVisible(true);
+			}
+		});
+	}
+}
